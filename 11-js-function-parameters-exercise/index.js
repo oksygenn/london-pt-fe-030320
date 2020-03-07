e81825ded47fe4732b3f85f166ea0eadb6283683
Oksana Kostenko <oxy@Oxymac.local>
2020-03-07 12:37:23 +0000

    Finished exercise (11)

--- 11-js-function-parameters-exercise/index.js
+++ 11-js-function-parameters-exercise/index.js
@@ -4,6 +4,10 @@
  * create a function "myName" which takes param(string)
  * of your name and console.log "My name is YOUR_NAME"
  */
+function myName(name) {
+    console.log(`My name is ${name}`);
+};
+myName("Oksana");
 
 /**
  * Exercise 2
@@ -11,7 +15,10 @@
  * create a function "myAge" which takes param(string)
  * of your age and console.log "I'm YOUR_AGE years old"
  */
-
+function myAge(age) {
+    console.log(`I'm ${age} years old`);
+};
+myAge(28);
 /**
  * Exercise 3
  *
@@ -19,9 +26,17 @@
  * your original city and country and console.log
  * "I'm from YOUR_CITY, YOUR_COUNTRY"
  */
-
+function aboutMe(city, country) {
+   console.log(`I'm from ${city}, ${country}`);
+}
+aboutMe('Zhmerynka', 'Ukraine');
 /**
  * Exercise 4
  * create a function "log12" which will take any 2 numbers
  * as params and log sum.
  */
+
+ function logSum( num1, num2) {
+     console.log(num1 + num2);
+ };
+ logSum(23, 23);
