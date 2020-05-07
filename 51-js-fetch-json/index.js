5c6986c4773440ec6049692ca31348f4ef15b43d
Oksana Kostenko <kseniavlad31@gmail.com>
2020-05-07 17:24:23 +0100

    ex 51 done

--- 51-js-fetch-json/index.js
+++ 51-js-fetch-json/index.js
@@ -17,7 +17,6 @@ const input = document.querySelector("input");
  * When you get a response, return an array of facts.
  */
 
-
 /**
  * Description of the application:
  *
@@ -25,3 +24,39 @@ const input = document.querySelector("input");
  * 1. click on a button "Get random facts"
  * 2. view 3 random facts in ".result" element
  */
+
+const fetchData = async () => {
+  const json = await fetch(
+    "https://cat-fact.herokuapp.com/facts"
+  ).then((response) => response.json());
+  return json.all;
+};
+
+const nodeFact = (randomFact) => {
+  const li = document.createElement("li");
+  const text = document.createElement("p");
+  text.className = "text";
+  text.innerText = randomFact.text;
+  const author = document.createElement("p");
+  author.className = "author";
+  const fullName = `${randomFact.user.name.first} ${randomFact.user.name.last}`;
+  author.innerText = fullName;
+  li.append(text, author);
+  return li;
+};
+
+const main = () => {
+  button.addEventListener("click", async () => {
+    const facts = await fetchData();
+
+    result.innerHTML = "";
+
+    for (let i = 0; i < 3; i++) {
+      const randomFact = facts[Math.floor(Math.random() * facts.length)];
+      const li = nodeFact(randomFact);
+      result.appendChild(li);
+    }
+  });
+};
+
+main();
