f6bac488df0d97b23589cb748da974e6f93f12b7
Oksana Kostenko <kseniavlad31@gmail.com>
2020-05-11 12:18:31 +0100

    ex 53 done

--- 53-js-fetch-rest/index.js
+++ 53-js-fetch-rest/index.js
@@ -1,23 +1,28 @@
 // set base URL to your json server
 // Ex: http://localhost:3000
-const baseURL = "http://localhost:3000";
+const baseURL = "http://127.0.0.1:3000";
+
+// const fetch = require("node-fetch");
 /**
  * BEFORE YOU START:
  * 1. Run `npm install -g json-server`
  * 2. In this directory, run `json-server db.json`
  * 		to serve your back end API
- * 3. In your broser, go to http://localhost:3000/comments,
+ * 3. In your browser, go to http://localhost:3000/comments,
  * 		to view the available data
  */
 
 /**
  * Exercise 1
  *
- * create an async function {getComments}, which 
+ * create an async function {getComments}, which
  * gets data from URL and returns the data as JS objects
  *
  * Note: test this function with an URL from your json-server API
  */
+const getComments = async () => {
+  return await fetch(`${baseURL}/comments`).then((response) => response.json());
+};
 
 /**
  * Exercise 2
@@ -25,12 +30,32 @@ const baseURL = "http://localhost:3000";
  * create an async function {postComment}, which takes {newComment} as an argument,
  * and posts it to the comments URL.
  *
- * This function should return the data recieved
+ * This function should return the data received
  * from the server if successful, or "Oops something went wrong!" if not.
  *
  * Don't forget to handle errors.
  */
+const postComment = async (newComment) => {
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newComment),
+  };
+  return fetch(`${baseURL}/comments`, configObject)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
+    })
+    .catch(() => "Oops something went wrong!");
+};
 
+// postComment({ body: "Damn it!" });
 /**
  * Exercise 3
  *
@@ -46,6 +71,32 @@ const baseURL = "http://localhost:3000";
  * Don't forget to handle errors.
  */
 
+const patchComment = async (comment, newCommentBody) => {
+  const newComment = { body: newCommentBody };
+  // trick to make comparison always pass by modifying original object :)
+  comment.random = Math.random();
+
+  const configObject = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newComment),
+  };
+
+  return fetch(`${baseURL}/comments/${comment.id}`, configObject)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
+    })
+    .catch(() => "Oops we couldn't update that!");
+};
+
+patchComment({ id: 21 }, "aaa");
 /**
  * Exercise 4
  *
@@ -57,7 +108,31 @@ const baseURL = "http://localhost:3000";
  *
  * Don't forget to handle errors.
  */
+const putComment = async (comment) => {
+  const configObject = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(comment),
+  };
 
+  try {
+    const response = await fetch(
+      `${baseURL}/comments/${comment.id}`,
+      configObject
+    );
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    return "Oops we couldn't update that!";
+  }
+};
+putComment({ id: 12, body: "meow" });
 /**
  * Exercise 5
  *
@@ -68,3 +143,29 @@ const baseURL = "http://localhost:3000";
  *
  * Don't forget to handle errors.
  */
+
+const deleteComment = async (comment) => {
+  const configObject = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(comment),
+  };
+
+  try {
+    const response = await fetch(
+      `${baseURL}/comments/${comment.id}`,
+      configObject
+    );
+    if (response.ok) {
+      return "Deleted!";
+    } else {
+      throw new error();
+    }
+  } catch (e) {
+    return "That could not be deleted!";
+  }
+};
+// deleteComment({ id: 3 });
