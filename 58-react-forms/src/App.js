bc848ac8124b48ec5977ec66d192f7d0164bcfdd
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-02 17:25:32 +0100

    ex 58 part 1

--- 58-react-forms/src/App.js
+++ 58-react-forms/src/App.js
@@ -16,7 +16,7 @@ import UserForm from "./Components/UserForm/UserForm";
  *
  * Exercise 2
  * 1. Create component UserForm.
- * 2. UserForm needs to have state {user} with properties: 
+ * 2. UserForm needs to have state {user} with properties:
  * 	- firstName, - lastName, - age, - gender.
  * 3. UserForm should contain 4 inputs:
  * - firstName(type: "text", class: firstName)
@@ -24,15 +24,36 @@ import UserForm from "./Components/UserForm/UserForm";
  * - age(type: "number", class: age)
  * - gender(type="select" class: gender,)
  * 4. When the value of any input changes, user state needs to reflect that.
- * 5. On form submit post your user to database. Validate data. 
+ * 5. On form submit post your user to database. Validate data.
  * If any value is missing, you shouldn't post user to database.
- * 
+ *
  * IMPORTANT: create div with class "state" and render there
  * all properties from {user} state.
  */
 
 const App = () => {
-	
+  const [inputText, setInputText] = useState("");
+  const doSomething = (event) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>{inputText}</h1>
+      <input
+        type="text"
+        className="first"
+        value={inputText}
+        onChange={doSomething}
+      />
+      <input
+        type="text"
+        className="second"
+        value={inputText}
+        onChange={doSomething}
+      />
+    </div>
+  );
 };
 
 export default App;
--- 58-react-forms/src/Components/UserForm/UserForm.js
+++ 58-react-forms/src/Components/UserForm/UserForm.js
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
 
 const UserForm = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    age: 0,
+    gender: "",
+  });
 
+  return (
+    <form>
+      <input type="text" className="firstName" />
+      <input type="text" className="lastName" />
+      <input type="number" className="age" />
+      <input type="select" className="gender" />
+    </form>
+  );
 };
 
 export default UserForm;
+/* 1. Create component UserForm.
+ * 2. UserForm needs to have state {user} with properties:
+ * 	- firstName, - lastName, - age, - gender.
+ * 3. UserForm should contain 4 inputs:
+ * - firstName(type: "text", class: firstName)
+ * - lastName(type: "text", class: lastName)
+ * - age(type: "number", class: age)
+ * - gender(type="select" class: gender,)
+ * 4. When the value of any input changes, user state needs to reflect that.
+ * 5. On form submit post your user to database. Validate data.
+ * If any value is missing, you shouldn't post user to database.
+ * */
