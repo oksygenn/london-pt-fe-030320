6da44247ffafe49bb35a8d0e159e5316c15b081d
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-15 21:31:35 +0100

    finished ui for savings

--- 63-react-bank/src/components/Savings/Savings.js
+++ 63-react-bank/src/components/Savings/Savings.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Savings.scss";
 import Balance from "../Wallet/Balance/Balance";
 import Header from "../Header/Header";
-import { getSavingsTransactions } from "../../API/mock";
+import { getSavingsTransactions, getSavingsBalance } from "../../API/mock";
 import Transactions from "../Wallet/Transactions/Transactions";
 
 const Savings = () => {
   return (
     <div className="savings">
       <Header signedIn={true} />
-      <Balance buttons={true} />
+      <Balance source={getSavingsBalance()} buttons={true} />
       <Transactions
         dateVisible={true}
         detailsExpandable={false}
--- 63-react-bank/src/components/Wallet/Balance/Balance.js
+++ 63-react-bank/src/components/Wallet/Balance/Balance.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Balance.scss";
 
 const Balance = (props) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
   return (
     <div className="balance_wrapper">
       <div className="balance_left_border colorized"></div>
       <div className="balance_internal_wrapper">
         <div className="balance_flex_item_1">
-          <p className="balance_numbers">{`${props.source.balance}`}</p>
+          <p className="balance_numbers">{props.source.balance}</p>
           <p className="balance_text">Balance</p>
         </div>
 
         <div className="balance_flex_item_2">
           {props.buttons ? (
-            <div className="balance_buttons">
-              <button className="balance_btn_1">pay in</button>
-              <button className="balance_btn_2">pay out</button>
-            </div>
+            <>
+              <button
+                onClick={toggleDetails}
+                className="balance_btn balance_btn_1"
+              >
+                pay in
+              </button>
+              <button
+                onClick={toggleDetails}
+                className="balance_btn balance_btn_2"
+              >
+                pay out
+              </button>
+            </>
           ) : (
             <>
               <div className="balance_user_avatar">
@@ -27,6 +43,16 @@ const Balance = (props) => {
           )}
         </div>
       </div>
+
+      {detailsVisible ? (
+        <div className="balance_expanded">
+          <div className="hr_line"></div>
+          <div className="balance_expanded_input_button">
+            <input type="text" placeholder="£ 500" />
+            <button className="balance_btn_3 colorized">Transfer</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
